8a83d1c60b8144aa49dbe735b9dd8e779a1cfc60
Sameersingh29 <sameerchandra12@gmail.com>
2024-09-03 00:32:54 +0530

    Updated staticfile rendering

--- Manju_Vatika_v1/settings.py
+++ Manju_Vatika_v1/settings.py
@@ -50,9 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Manju_Vatika_v1.urls'
+
 
 TEMPLATES = [
     {
@@ -119,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
