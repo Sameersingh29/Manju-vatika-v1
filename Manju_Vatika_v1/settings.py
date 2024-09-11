28106e9d12240cd51efc3f635b83fd6e16d83ccf
Sameer <73021790+Sameersingh29@users.noreply.github.com>
2024-09-11 14:54:55 +0530

    Update settings.py

--- Manju_Vatika_v1/settings.py
+++ Manju_Vatika_v1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-u!6purvvemwm*5p6=e9_^et+p!ik+t&+gx$o9k-acs#pvx34mc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['manju-vatika-v1.onrender.com','127.0.0.1','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['manju-vatika-v1.onrender.com','127.0.0.1','web-production-1a183.up.railway.app']
 
 
 # Application definition
